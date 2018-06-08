132f55f6002a9c668940c32727853948b553279d
Techworker <benjaminansbach@gmail.com>
2018-06-08 12:35:58 +0200

    Changed key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(100);
 
         if(getenv('APP_ENV') === 'prod') {
             \URL::forceScheme('https');
