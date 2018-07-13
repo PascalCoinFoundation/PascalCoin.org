a4f1173d97311bd73329a2418a2bedfddf0cc430
Techworker <benjaminansbach@gmail.com>
2018-07-13 10:09:10 +0200

    Added cache busting for css and js resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,4 +22,5 @@ if (!mix.inProduction()) {
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css');
+    .sass('resources/assets/sass/admin.scss', 'public/css')
+    .version();
