1649dc548c50bb0887c8e3a4cc72b9940b0be71f
Techworker <benjaminansbach@gmail.com>
2019-01-23 16:33:33 +0100

    activated captcha again..

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -179,7 +179,7 @@ class IndexController extends Controller
             'name' => 'required|max:255',
             'email' => 'required|email',
             'message' => 'required',
-            //'g-recaptcha-response' => 'required|captcha'
+            'g-recaptcha-response' => 'required|captcha'
         ]);
 
         if ($validator->fails()) {
