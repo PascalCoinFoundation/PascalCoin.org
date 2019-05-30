fa07b65eb77f197a25317a27cc08aff8a5680526
PascalCoin <contact@pascalcoin.org>
2019-05-30 11:50:07 -0500

    add versions to roadmap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -373,150 +373,100 @@ PascalCoin achieves all this by introducing a new cryptographic data-structure k
             </div>
             <div class="row">
                 <div class="col-sm-12">
-                    <h3>Accomplished</h3>
+                    <h3>Version 1</h3>
                     <div>
                         <ul class="roadmap">
-                            @dt($dtGroup, 'roadmap_done', '
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>Initial launch</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>Decentralised Development Process (PIP)</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: Miner infrastructure</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: In-protocol account exchanging</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: Infinite Scaling (100 block chain)</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: Checkpointing</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: Account Names & Types</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: Mobile/light-client support</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: 50% inflation reduction</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>CORE: Add 20% developer reward (PIP-0011)</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>VOTING: Blockchain voting explorer</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>VOTING: SafeBox Voting (phase 1)</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>ANON: In-protocol PASC & PASA exchange</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>GUI: New Wallet (with OS X support)</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>GUI: Translation Portal</p>
-</li>
-<li>
-    <i class="fa fa-check-square"></i>
-    <p>DAO: 90% developer reward open to community </p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>CORE: CPU & ASIC Resistant mining (PIP-0009)</p>
-</li>
-
-<li>
-    <i class="fa fa-square"></i>
-    <p>CORE: Layer-2 operation support</p>
-</li>')
+                            @dt($dtGroup, 'roadmap_version_1', '')
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-12">
-
-                    <h3>Our Goals</h3>
+                    <h3>Version 2</h3>
                     <div>
-                        <ul class="roadmap goals">@dt($dtGroup, 'roadmap_goals', '
-<li>
-    <i class="fa fa-square"></i>
-    <p>MULTISIG: Schnorr crypto-library</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>CORE: Checkpoint Torrenting</p></li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>CORE: Account Data</p></li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>CORE: Refactoring & optimization</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>VOTING: Voting Protocol (phase 2)</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>MULTISIG: Schnorr aggregate signatures</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>ANON: Private Balances & Payments (ZK-SNARKs)</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>GUI: V3 Full Translations</p></li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>GUI: Mobile Wallets (iOS & Android)</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>LAYER2: Delegated Proof-of-Stage Overlay Network (Smart-Contracts)</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>LAYER2: Private Send Protocol</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>LAYER2: PASC Data Storage Network (DSN)</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>LAYER2: PascalTokens V1 </p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>LAYER2: PascalDEX (decentralised exchange)</p>
-</li>
-<li>
-    <i class="fa fa-square"></i>
-    <p>LAYER2: PascalMessenger (PASC MSG) </p>
-</li>')
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_version_2', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Version 3</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_version_3', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Version 4</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_version_4', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Version 5</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_version_5', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Version 6</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_version_6', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Version 7</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_version_7', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>R&amp;D Track</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_rd_track', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>WIP Track</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_wip_track', '')
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Production Assets</h3>
+                    <div>
+                        <ul class="roadmap">
+                            @dt($dtGroup, 'roadmap_production_assets', '')
                         </ul>
                     </div>
                 </div>
